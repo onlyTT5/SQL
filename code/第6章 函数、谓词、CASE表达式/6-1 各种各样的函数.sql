3d2363d3377f955c487bdf57c06366f090c99384
onlyTT5 <2218201514@qq.com>
2023-01-10 16:02:00 +0800

    commit file

--- code/第6章 函数、谓词、CASE表达式/6-1 各种各样的函数.sql
+++ code/第6章 函数、谓词、CASE表达式/6-1 各种各样的函数.sql
@@ -35,3 +35,57 @@ FROM SampleMath;
 
 SELECT m, n, ROUND(m, n) AS round_col
 FROM SampleMath;
+
+CREATE TABLE SampleStr
+(
+    str1 VARCHAR(40),
+    str2 VARCHAR(40),
+    str3 VARCHAR(40)
+);
+
+BEGIN TRANSACTION;
+
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('opx', 'rt', NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('abc', 'def', NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('山田', '太郎', '是我');
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('aaa', NULL, NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES (NULL, 'xyz', NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('@!#$%', NULL, NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('ABC', NULL, NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('aBC', NULL, NULL);
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('abc太郎', 'abc', 'ABC');
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('abcdefabc', 'abc', 'ABC');
+INSERT INTO SampleStr (str1, str2, str3) VALUES ('micmic', 'i', 'I');
+
+COMMIT;
+
+SELECT * FROM SampleStr;
+
+SELECT str1, str2, str1 + str2 AS str_concat
+FROM SampleStr;
+
+SELECT str1, str2, str3, str1 + str2 + str3 AS str_concat
+FROM SampleStr;
+
+SELECT str1, LEN(str1) AS len_str
+FROM SampleStr;
+
+SELECT str1, LOWER(str1) AS low_str
+FROM SampleStr
+WHERE str1 IN ('ABC', 'aBC', 'abc', '山田');
+
+SELECT str1, str2, str3, REPLACE(str1, str2, str3) AS rep_str
+FROM SampleStr;
+
+SELECT str1, SUBSTRING(str1, 3, 2)
+FROM SampleStr;
+
+SELECT str1, UPPER(str1) AS up_str
+FROM SampleStr
+WHERE str1 IN ('ABC', 'aBC', 'abc', '山田');
+
+SELECT CAST(CURRENT_TIMESTAMP AS DATE) AS CUR_DATE;
+
+SELECT CAST(CURRENT_TIMESTAMP AS TIME) AS CUR_TIME;
+
+SELECT CURRENT_TIMESTAMP;
